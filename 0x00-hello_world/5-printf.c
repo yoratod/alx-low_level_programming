5d1f7ffe2250d3b2b394efc5082815a55a08d1ec
yoratod <yordanosayelet@gmail.com>
2023-02-16 06:55:29 -0800

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include main.h
 /**
  * Descreption:  main - prints out a string to stdout
  * Return: 0 if success.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 ~

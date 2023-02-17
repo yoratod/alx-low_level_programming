f39b48a13ce9af008bf04badb59262f9c2b551e6
yoratod <yordanosayelet@gmail.com>
2023-02-17 02:52:59 -0800

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -S -masm=intel $CFILE
 gcc 6-size.c -m32 --o size32 2> /temp/32
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c

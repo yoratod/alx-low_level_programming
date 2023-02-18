6f1964e48ed5ed582feb9b2a483ffe527de9ce47
yoratod <yordanosayelet@gmail.com>
2023-02-18 02:04:46 -0800

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Descreption:  main - prints out a string to stdout
- * Return: 0 if success.
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of arti,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of arti,\n");
+	return (0);
 }
-~

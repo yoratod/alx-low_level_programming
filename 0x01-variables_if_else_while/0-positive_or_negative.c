80142e82551c8b9f7a4df8c2d90d48a5c1d2e214
yoratod <yordanosayelet@gmail.com>
2023-02-18 02:29:00 -0800

    m

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - Always print the correct output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 2;
+
+	if (n > 0)
+	{
+		printf("positive");
+	}
+	else
+	{
+		printf("negative");
+	}
+	return (0);
+}

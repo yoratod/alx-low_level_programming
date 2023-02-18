3fb3776a8b0b82d8b9311abe856e520e50d0b97b
yoratod <yordanosayelet@gmail.com>
2023-02-18 02:00:30 -0800

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * Descreption:  main - prints out a string to stdout
  * Return: 0 if success.
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of arti,");
+printf("with proper grammar, but the outcome is a piece of arti,\n");
 return (0);
 }
 ~

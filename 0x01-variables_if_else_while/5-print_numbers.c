8d2ba04dda1af81c4025279c94bfeda400ba5278
yoratod <yordanosayelet@gmail.com>
2023-02-20 03:32:09 -0800

    m

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int num;
-	
-	for(num=0; num<10;num++)
-		
-		printf("%d",num);
-	return 0; 
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf('\n');
+	return (0);
 }

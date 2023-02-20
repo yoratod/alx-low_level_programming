9f706ef71f05a5b610dd13e21967ef6c19af79c1
yoratod <yordanosayelet@gmail.com>
2023-02-20 03:59:17 -0800

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(voidi)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		if (ch != 'e' && ch != 'q')
-		{
 			putchar(ch);
-		}
 	putchar('\n');
 	return (0);
 }

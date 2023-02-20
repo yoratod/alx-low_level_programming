4f60a9162eac12736c136240ec352c719eba11ec
yoratod <yordanosayelet@gmail.com>
2023-02-20 00:28:54 -0800

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char ch;
+	char letter;
 
-	for (ch = 'a' ; ch < 'z' ; ch++)
-		putchar(ch);
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+		putchar(letter);
 		putchar("\n");
 	return (0);
 }

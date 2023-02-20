e39616f50d498cf7d5c924064966c96c04451d77
yoratod <yordanosayelet@gmail.com>
2023-02-20 00:53:13 -0800

    m

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,18 @@
  */
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
+
 		putchar(ch);
-	for (ch= 'A' ;ch <= 'Z' ; ch++)
-                putchar(ch);
-        putchar('\n');
 
-        return (0);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
 }
 

9703e0e1a69c7ad9c6cd5775460e8c810540b9b2
yoratod <yordanosayelet@gmail.com>
2023-02-23 02:24:57 -0800

    m

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * _isupper - check the code.
  *
  * Return: Always 0.
  */
 #include <stdio.h>
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,8 @@
+/**
+ * mul - check the code
+ *
+ * Return: Always a*b.
+ */
 int mul(int a, int b)
 {
 	return (a * b);

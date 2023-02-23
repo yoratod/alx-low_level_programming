4b22928ad9cc01e53bd087c470d5e628bab483b6
yoratod <yordanosayelet@gmail.com>
2023-02-23 02:41:35 -0800

    m

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 /**
  * _isdigit - check the code
  *
+ * @c: input
  * Return: 0 and 1.
  */
 int _isdigit(int c)
 {
-	if (c <= 0; c >= 9)
+	if (c <= 48; c >= 58)
 		return (1);
 	else
 		return (0);

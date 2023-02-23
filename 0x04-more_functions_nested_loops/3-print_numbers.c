5cf351badbeddd568bd4b3373f0fada8e87494d6
yoratod <yordanosayelet@gmail.com>
2023-02-23 03:56:52 -0800

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,15 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void print_numbers(void)
 {
 	char n;
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		_putchar(n + "0");
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }

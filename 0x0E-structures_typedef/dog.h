d3910e0803188906e0ec8db472a56f63c6b27041
yoratod <yordanosayelet@gmail.com>
2023-03-21 00:16:32 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,1 @@
-#ifndef _HEADER_
-#define _HEADER_
-#include <stdio.h>
-#include "dog.h"
-
-/**
- * dog - check the code by defining new struct
- *
- * Return: Always 0.
- */
-struct dog
-{
-	char  *name;
-	float age;
-	char *owner;
-};
-#endif
 

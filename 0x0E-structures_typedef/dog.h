5194451be61095093727b0d861979e5c43a60535
yoratod <yordanosayelet@gmail.com>
2023-03-21 01:33:12 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,20 @@
+#ifdef _HEADER_
 #include <stdio.h>
 #include "dog.h"
 /**
  * main - check the code by defining new struct
  *
+ * @dog :return d;
+ * @name : include *name
  * Return: Always 0.
  */
-int main(struct dog->d,char  *name,float age, char *owner)
+int main(struct dog *d,char  *name,float age, char *owner)
 {
-        d -> *name;
-        d ->  age;
-        d ->  *owner;
+	if(d != null)
+	{
+        d-> *name;
+        d->  age;
+        d->  *owner;
+	}
 };
+#endif

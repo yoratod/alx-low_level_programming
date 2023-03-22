269ec7b254ad105cdc3a37de7dc56726d49c56ee
yoratod <yordanosayelet@gmail.com>
2023-03-22 03:19:10 -0700

    m

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the name
+ * @f: name of the f
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+}

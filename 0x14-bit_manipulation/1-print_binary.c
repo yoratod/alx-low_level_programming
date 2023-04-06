352d5ddd4208834130cb4c92ef3af77f84e7e761
yoratod <yordanosayelet@gmail.com>
2023-04-06 04:10:16 -0700

    m

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,9 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1;  /* create a mask with the lowest bit set to 1 */
+    if (n > 1)  /* base case: if n is 0 or 1, just print it */
+        print_binary(n >> 1);
 
-    /* shift the mask left until the highest bit of n is reached */
-    while ((mask << 1) <= n)
-        mask <<= 1;
-
-    /* print the binary representation of n by checking each bit in the mask */
-    while (mask > 0)
-    {
-        if (n & mask)
-            _putchar('1');
-        else
-            _putchar('0');
-
-        mask >>= 1;  /* shift the mask right to check the next bit */
-    }
+    _putchar((n & 1) ? '1' : '0');  /* print the last bit of n */
 }
 

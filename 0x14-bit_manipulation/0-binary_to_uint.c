273a88cb5eca78bad3741d0e8d14a863d820b2be
yoratod <yordanosayelet@gmail.com>
2023-04-06 04:32:20 -0700

    m

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int n = 0;
 
-	if (!b)
+	if (b == null)
 		return (0);
 
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		num = (num << 1) + (*b++ - '0');
+		n = (n << 1) + (*b++ - '0');
 	}
 
-	return (num);
+	return (n);
 }
 
